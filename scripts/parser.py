bc874d9aaa82a80531f79e080fa2b5590ddd6dbc
WorldWideWest <dzeno.dzafic@windowslive.com>
2021-04-03 12:22:08 +0200

    Deviding the logic and the design

--- scripts/parser.py
+++ scripts/parser.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 columns = ['City', 'Customer type', 'Gender','Product line', 'Unit price',
            'Quantity', 'Tax 5%', 'Total', 'Date', 'Payment', 'cogs',
@@ -6,12 +7,33 @@ columns = ['City', 'Customer type', 'Gender','Product line', 'Unit price',
 
 
 
-def Import(path, columns = columns):
-    data = pd.read_csv(path)
-    return data[columns]
+class Application:
+    def __init__(self, columns = columns):
+        self.columns = columns
 
-def Mapper(dataFrame, column, mapKeys):
-    dataFrame[column] = dataFrame[column].map(mapKeys)
-    return dataFrame
+    def Import(self, path):
+        data = pd.read_csv(path)
+        return data[self.columns]
+
+    def PiePlot(self, dataFrame, column, colors, title):
+        data = []
+        values = dataFrame.groupby(column).count()
+        indicies = [col for col in values.index]
+            
+        for index in indicies:
+            data.append(dataFrame[dataFrame[column] == index].count()[0])
+                    
+        fig, ax = plt.subplots()
+        plt.pie(data, colors = colors, autopct = '%1.2f%%', startangle = 90, textprops = {'color': 'white'})
+                                
+        fig.patch.set_facecolor('blue')
+        fig.patch.set_alpha(0)
+
+        plt.title(f'{ title }', fontdict = {'fontsize': 14, 'fontweight': 'bold', 'color': 'white'})
+                                            
+        plt.legend(indicies)
+
+        return fig
+
 
 
